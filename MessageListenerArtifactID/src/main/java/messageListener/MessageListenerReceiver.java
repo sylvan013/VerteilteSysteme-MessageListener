13f7fb356dc51f9b6706ca5017ec98d73065cd3d
sylvan013 <kai.keppler@web.de>
2024-03-18 18:49:23 +0100

    feat: format code

--- MessageListenerArtifactID/src/main/java/messageListener/MessageListenerReceiver.java
+++ MessageListenerArtifactID/src/main/java/messageListener/MessageListenerReceiver.java
@@ -4,74 +4,73 @@ import javax.naming.*;
 import javax.jms.*;
 
 public class MessageListenerReceiver {
-	
+
 	// Member-Variables
 	InitialContext initialContext;
 	ConnectionFactory connectionFactory;
 	Connection connection;
 	Session session;
-	
+
 	Queue queue;
 	MessageConsumer queueConsumer;
-	
+
 	Topic topic;
 	MessageConsumer topicConsumer;
-	
+
 	// Connect to Message-Broker
 	public void connectToMessageBroker() throws Exception {
 		initialContext = new InitialContext();
 
-        connectionFactory = (ConnectionFactory)
-                initialContext.lookup("/ConnectionFactory");
+		connectionFactory = (ConnectionFactory) initialContext.lookup("/ConnectionFactory");
 
-        connection = connectionFactory.createConnection();
+		connection = connectionFactory.createConnection();
 
-        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 	}
-	
+
 	// Access Point-To-Point Queue
 	public void accessPointToPointQueue() throws Exception {
 		queue = (Queue) initialContext.lookup("/queue/ExampleQueue");
 		queueConsumer = session.createConsumer(queue);
 	}
-	
+
 	// Access Topic-Orientated Queue
 	public void accessTopicQueue() throws Exception {
 		topic = (Topic) initialContext.lookup("/topic/ExampleTopic");
 		topicConsumer = session.createConsumer(topic);
 	}
-	
+
 	// Get Messages and Print them Out
 	public void getAndPrintMessages() throws Exception {
 		TextMessage message0 = (TextMessage) queueConsumer.receive();
 		TextMessage message1 = (TextMessage) topicConsumer.receive();
-		
+
 		queueConsumer.setMessageListener(new MessageListener() {
-			
+
 			@Override
 			public void onMessage(Message message) {
 				System.out.println(message);
 			}
 		});
-		
+
 		topicConsumer.setMessageListener(new MessageListener() {
-			
+
 			@Override
 			public void onMessage(Message message) {
 				System.out.println(message);
 			}
 		});
 	}
-	
+
 	// Close all Connections
 	public void closeConnections() throws Exception {
 		initialContext.close();
 		connection.close();
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		MessageListenerReceiver messageListenerReceiver = new MessageListenerReceiver();
-		
+
 		messageListenerReceiver.connectToMessageBroker();
 		messageListenerReceiver.accessPointToPointQueue();
 		messageListenerReceiver.accessTopicQueue();
--- MessageListenerArtifactID/src/main/java/messageListener/MessageListenerSender.java
+++ MessageListenerArtifactID/src/main/java/messageListener/MessageListenerSender.java
@@ -7,67 +7,69 @@ import java.util.Properties;
 import javax.jms.*;
 
 public class MessageListenerSender {
-	
+
 	// Member-Variables
 	InitialContext initialContext;
 	ConnectionFactory connectionFactory;
 	Connection connection;
 	Session session;
-	
+
 	Queue queue;
 	MessageProducer queueMessageProducer;
-	
+
 	Topic topic;
 	MessageProducer topicMessageProducer;
-	
+
 	// Connect to Message-Broker
 	public void connectToMessageBroker() throws Exception {
-	    Properties props = new Properties();
-	    props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
-	    props.setProperty(Context.PROVIDER_URL, "tcp://localhost:61616");
-	    initialContext = new InitialContext(props);
+		Properties properties = new Properties();
+        properties.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+        properties.setProperty(Context.PROVIDER_URL, "tcp://localhost:61616");
 
-	    connectionFactory = (ConnectionFactory) initialContext.lookup("ConnectionFactory");
-	    connection = connectionFactory.createConnection();
-	    session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        initialContext = new InitialContext(properties);
+
+        connectionFactory = (ConnectionFactory) initialContext.lookup("ConnectionFactory");
+
+        connection = connectionFactory.createConnection();
+
+        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 	}
 
-	
 	// Access Point-To-Point Queue
 	public void accessPointToPointQueue() throws Exception {
 		queue = (Queue) initialContext.lookup("/queue/ExampleQueue");
-        queueMessageProducer = session.createProducer(queue);
+		queueMessageProducer = session.createProducer(queue);
 	}
-	
+
 	// Access Topic-Orientated Queue
 	public void accessTopicQueue() throws Exception {
 		topic = (Topic) initialContext.lookup("/topic/ExampleTopic");
-        topicMessageProducer = session.createProducer(topic);
+		topicMessageProducer = session.createProducer(topic);
 	}
-	
+
 	// Send Messages
 	public void sendMessages() throws Exception {
 		Message message0 = session.createTextMessage("Hello World!");
-        queueMessageProducer.send(message0);
-        
-        Message message1 = session.createTextMessage("Goodbye World!");
-        topicMessageProducer.send(message1);
+		queueMessageProducer.send(message0);
+
+		Message message1 = session.createTextMessage("Goodbye World!");
+		topicMessageProducer.send(message1);
 	}
-	
+
 	// Close all Connections
 	public void closeConnections() throws Exception {
 		if (initialContext != null) {
-            initialContext.close();
-        }
+			initialContext.close();
+		}
 
-        if (connection != null) {
-            connection.close();
-        }
+		if (connection != null) {
+			connection.close();
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
 		MessageListenerSender messageListenerSender = new MessageListenerSender();
-		
+
 		messageListenerSender.connectToMessageBroker();
 		messageListenerSender.accessPointToPointQueue();
 		messageListenerSender.accessTopicQueue();
